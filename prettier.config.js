fc6a45a410989aaefbfaf09e0f39494320df70fe
Yale Araujo <yale850@gmail.com>
2025-05-01 23:53:35 -0300

    my first commit

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: "all",
+  arrowParens: "always",
+  semi: false,
+  bracketSpacing: true,
+  endOfLine: "auto",
+}
