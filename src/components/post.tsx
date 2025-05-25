ca349391c1730ef3665970785e7a3774089363f9
Yale Araujo <yale850@gmail.com>
2025-05-25 11:08:27 -0300

    feat: created the comment form

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -32,6 +32,17 @@ export const Post = () => {
           <a href="">#nlw</a> {' '} <a href="">#rocketseat</a>{' '}
         </p>
       </div>
+
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder='Deixe um comentário'/>
+
+        <footer>
+        <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
